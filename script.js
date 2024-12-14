f7337f9a59dce2e99c85674408d4e95753b110af
r0ry64 <rorybeatz@gmail.com>
2024-12-14 01:11:22 -0500

    created products

--- script.js
+++ script.js
@@ -48,3 +48,9 @@ class Store {
         return this.inventory.find(product => product.name === name) || null;
     }
 }
+
+const hwnPizza = new ProductProperties("Hawaiian Pizza", 10.0, 2);
+const chickenNug = new ProductProperties("Chicken Nuggets", 0.5, 30);
+const hotDog = new ProductProperties("Hot Dog", 1.0, 100);
+const milk = new PerishableProductProperties("Milk", 1.5, 10, "2024-12-31");
+const avocado = new PerishableProductProperties("Avocado", 4.0, 5, "2024-12-15");
